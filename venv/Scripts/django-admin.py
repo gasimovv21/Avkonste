6a9cc4d70f9bd881036538f3d26914f0181a4f59
gasimovv21 <framee21@yandex.com>
2023-03-05 18:20:22 +0400

    Killed venv folder

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\gasimovv21\documents\dev\workprojects\avkonste\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

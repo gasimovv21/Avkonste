a8b1a2ed37b8996e4038f01e7cc0024c42e30609
gasimovv21Server <gasimoweltun@gmail.com>
2023-03-05 21:15:53 +0000

    Fix static for nginx

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
 handler404 = handler404
 handler403 = handler403
 handler505 = handler505

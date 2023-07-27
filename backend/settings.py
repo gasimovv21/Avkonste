f1ad3b1c325c6cf3b3ba8c5491a3879987cdaac1
gasimovv21 <gasimoweltun@gmail.com>
2023-07-27 20:05:02 +0400

    Returned default settings.py for server

--- backend/settings.py
+++ backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv()
 
 SECRET_KEY = 'coevnf71tncg7+!zk+5g8&av$ad6vfjsr3m7jcj3r8fv1!^^@'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['165.232.122.174', 'localhost', 'avkonste.com', '*'] 
 
@@ -54,23 +54,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'avkonste',
-#         'USER': 'avkonste',
-#         'PASSWORD': 'avkonste2101',
-#         'HOST': '127.0.0.1',
-#         'PORT': 5432
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'avkonste',
+        'USER': 'avkonste',
+        'PASSWORD': 'avkonste2101',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -96,11 +96,11 @@ USE_I18N = True
 USE_L10N = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
